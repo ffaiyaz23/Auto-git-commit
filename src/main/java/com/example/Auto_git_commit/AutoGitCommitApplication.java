f82b59d9cd8116895ea0800d5641231711d24d29
Faiz Faiyaz <forcourse23@gmail.com>
2025-04-13 21:51:48 +0530

    Intial commit

---
+++ src/main/java/com/example/Auto_git_commit/AutoGitCommitApplication.java
@@ -0,0 +1,13 @@
+package com.example.Auto_git_commit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoGitCommitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoGitCommitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Auto_git_commit/AutoGitCommitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Auto_git_commit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoGitCommitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
